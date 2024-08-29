1d29aa1fcf1efbaed15397f9b7fb8a6275bfce70
HenriqueVogelsanger <henriquevogelsanger2008@gmail.com>
2024-08-28 22:30:25 -0300

    finalizando

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: 'home',
+      path: '/home',
       name: 'home',
       component: HomeView
     },
